4e6561b021da31afd9ff74f02b9d9c3f4efe8756
ESFCU-IS-Dev <117106241+ESFCU-IS-Dev@users.noreply.github.com>
2023-10-18 16:44:43 -0400

    Add files via upload

---
+++ TestClient/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TestClient")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("aopc")> 
+<Assembly: AssemblyProduct("TestClient")> 
+<Assembly: AssemblyCopyright("Copyright © aopc 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ddb3d0de-8b90-4700-8ac9-7b2611a28093")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ TestClient/TestClient.vb
@@ -0,0 +1,7 @@
+﻿Public Class TestClient
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim oAlertEngine As New USEcology.AlertEngine
+        oAlertEngine.DoWork()
+    End Sub
+End Class
